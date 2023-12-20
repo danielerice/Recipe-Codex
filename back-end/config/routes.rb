f0ee045b2bc7dc2140c3baafb550883b1416078c
Daniel Rice <danielrice5627@gmail.com>
2023-12-20 10:58:02 -0800

    fixing controllers'

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   resources :recipe_books_recipes
   resources :recipes
   resources :recipe_books
+
+
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  get '/user_recipes/:n', to: 'users#user_recipes'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
