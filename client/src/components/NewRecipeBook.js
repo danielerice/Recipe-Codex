7443e979bb3541accb9c7a7c501fdbb1db686a40
Daniel Rice <danielrice5627@gmail.com>
2023-12-31 11:32:41 -0800

    fixed the css for newrecipe form

--- client/src/components/NewRecipeBook.js
+++ client/src/components/NewRecipeBook.js
@@ -45,16 +45,27 @@ function NewRecipeBook () {
 
     
         return (
-            <div className="center">
-            <div className="newRecipeCard">
-                <form onSubmit={postNewRecipeBook}>
-                    <label>Name:</label>
-                    <input type="text" id="textinput" placeholder="This is an example!" onChange={(e) => setName(e.target.value)} value={name}></input>
-                    <label>Description:</label>
-                    <textarea id="directions" type="input" placeholder="All my faves!" onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
-                    <button type="submit" >Submit</button>
-                </form>
-            </div>
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-6 gy-5">
+                        <div class="mb-3">
+                            <label for="textinput" class="form-label">Name</label>
+                            <input class="form-control" type="text" id="textinput" placeholder="This is an example!" onChange={(e) => setName(e.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="directions" class="form-label">Directions</label>
+                            <textarea class="form-control" id="directions" type="input" placeholder="All my faves!" onChange={(e) => setDescription(e.target.value)} rows="3"></textarea>
+                        </div>
+                        <button className="btn btn-primary" type="submit" >Submit</button>
+                        {/* <form className="form-control" onSubmit={postNewRecipeBook}>
+                            <label>Name:</label>
+                            <input type="text" id="textinput" placeholder="This is an example!" onChange={(e) => setName(e.target.value)} value={name}></input>
+                            <label>Description:</label>
+                            <textarea id="directions" type="input" placeholder="All my faves!" onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
+                            <button className="btn btn-primary" type="submit" >Submit</button>
+                        </form> */}
+                    </div>
+                </div>
             </div>
 
     )
