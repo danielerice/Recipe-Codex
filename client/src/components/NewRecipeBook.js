72c121b0c3409c1c88699524d22a71b5b58c7e9c
Daniel Rice <danielrice5627@gmail.com>
2024-01-10 12:05:59 -0800

    changed directions to description

--- client/src/components/NewRecipeBook.js
+++ client/src/components/NewRecipeBook.js
@@ -51,7 +51,7 @@ function NewRecipeBook () {
                             <input class="form-control" type="text" id="textinput" placeholder="This is an example!" onChange={(e) => setName(e.target.value)}/>
                         </div>
                         <div class="mb-3">
-                            <label for="directions" class="form-label">Directions</label>
+                            <label for="directions" class="form-label">Description</label>
                             <textarea class="form-control" id="directions" type="input" placeholder="All my faves!" onChange={(e) => setDescription(e.target.value)} rows="3"></textarea>
                         </div>
                         <button className="btn btn-primary" type="submit" onClick={(e) => postNewRecipeBook()} >Submit</button>
